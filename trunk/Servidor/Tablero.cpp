c35c5e9eb8dd121b4c96b23642933837c2707abe
debbie1new.world@gmail.com <debbie1new.world@gmail.com@b4b15c0e-5f7a-2ca3-c1f0-97e55bb4af40>
2013-11-19 05:44:40 +0000

    nivel

--- trunk/Servidor/Tablero.cpp
+++ trunk/Servidor/Tablero.cpp
@@ -1,9 +1,17 @@
 #include "Tablero.h"
+#define ESPACIO '0'
 
-
-Tablero::Tablero(int tam)
+Tablero::Tablero(Dimension& tam, char** estructura)
     : Matriz<Celda>(tam), jugada_en_curso(NULL){
     modificados = new Lista<Dimension*>(true);
+    for(int i=0; i < tam.y(); i++){
+        for(int j=0; j < tam.x(); j++){
+            if(estructura[i][j] == ESPACIO){
+                (*this)[j][i].setTipo(VACIO);
+                (*this)[j][i].setColor(VACIO);
+            }
+        }
+    }
 }
 
 Tablero::~Tablero(){
@@ -101,7 +109,7 @@ int Tablero::borrarColumna(Dimension& dest, Dimension& origen){
 
         if (tipo == MINIBARH){
             Dimension principio(actual.x(), 0);
-            Dimension final(actual.x(), tamanio-1);
+            Dimension final(actual.x(), tamanio->y()-1);
             cant += borrarFila(principio, final);
         } else if (tipo == MINIBARV) {
             hay_barv = true;
@@ -117,12 +125,12 @@ int Tablero::borrarColumna(Dimension& dest, Dimension& origen){
         }
     }
     if(hay_barv){
-        Dimension principio(tamanio-1, origen.y());
-        Dimension fin(tamanio - dest.x() + origen.x() -1, dest.y());
+        Dimension principio(tamanio->x() -1, origen.y());
+        Dimension fin(tamanio->x() - dest.x() + origen.x() -1, dest.y());
         cant += borrarSegmentosCol(principio, fin);
 
         Dimension primero(0, dest.y());
-        Dimension ultimo(tamanio-1, dest.y());
+        Dimension ultimo(tamanio->x() -1, dest.y());
         cant += borrarSegmentosCol(primero, ultimo, false);
     }
     return cant;
@@ -141,10 +149,10 @@ int Tablero::borrarFila(Dimension& inicio, Dimension& fin){
             Dimension fin(actual.x(), inicio.y() -1);
             if (principio != inicio)
                 cant += borrarFila(principio, fin);
-            max = tamanio-1;
+            max = tamanio->y() -1;
         } else if (tipo == MINIBARV) {
             Dimension origen(-1, actual.y());
-            Dimension dest(tamanio-1, actual.y());
+            Dimension dest(tamanio->x() -1, actual.y());
             cant += borrarSegmentosCol(dest, origen);
         } else if (tipo == VACIO) {
             continue;
@@ -168,7 +176,7 @@ void Tablero::borrarLinea(Dimension& inicio, Dimension& fin, bool por_caida){
 int Tablero::buscarConfEspecial(Dimension& pos, int orientacion, Dimension& inicial, Dimension& final){
     int fila = pos.x(), col = pos.y();
     int inicio = (orientacion == FILA) ? ((col-2 >= 0) ? col-2 : 0) : ((fila-2  >= 0) ? fila-2 : 0);
-    int fin = (orientacion == FILA) ? ((col+2 < tamanio) ? col+2 : tamanio-1) : ((fila+2  < tamanio) ? fila+2 : tamanio-1);
+    int fin = (orientacion == FILA) ? ((col+2 < tamanio->y()) ? col+2 : tamanio->y() -1) : ((fila+2  < tamanio->x()) ? fila+2 : tamanio->x() -1);
     int i = inicio-1, cant = 0;
     int& fila_n = (orientacion == FILA) ? fila : i;
     int& col_n = (orientacion == FILA) ? i : col;
@@ -218,23 +226,23 @@ void Tablero::explosionEstrella(){
         borrarColumna(pos_est, pos_est);
     }
 
-    for(int i=0; i < tamanio; i++){
+    for(int i=0; i < tamanio->y(); i++){
         if (tipo_orig == ESTRELLA){
-            Dimension inicio(tamanio-1, i);
+            Dimension inicio(tamanio->x() -1, i);
             Dimension fin(0, i);
             cant += borrarSegmentosCol(inicio, fin);
             continue;
         }
-        for(int j=0; j < tamanio; j++){
+        for(int j=0; j < tamanio->x(); j++){
             Dimension pos_actual(j, i);
 
             if((*this)[pos_actual].getColor() == color_orig) {
                 if(tipo_orig == MINIBARH || tipo_orig == MINIBARV){
                     Dimension inicio1(pos_no_est.x(), 0);
-                    Dimension fin1(pos_no_est.x(), tamanio-1);
+                    Dimension fin1(pos_no_est.x(), tamanio->y() -1);
                     cant += borrarFila(inicio1, fin1) -1;
 
-                    Dimension inicio2(tamanio-1, pos_no_est.y());
+                    Dimension inicio2(tamanio->x() -1, pos_no_est.y());
                     Dimension fin2(0, pos_no_est.y());
                     cant += borrarSegmentosCol(inicio2, fin2);
                 } else {
@@ -261,9 +269,9 @@ bool Tablero::reemplazarOriginal(int len_linea, Dimension& pos, int orientacion)
     return true;
 }
 
-bool enRango(Dimension& pos, int tam){
+bool enRango(Dimension& pos, Dimension& tam){
     if(!pos.esValida()) return false;
-    return ((pos.x()<tam) && (pos.y() <tam));
+    return ((pos.x() < tam.x()) && (pos.y() < tam.y()));
 }
 
 bool Tablero::hayMovimiento(Dimension& pos){
@@ -275,7 +283,7 @@ bool Tablero::hayMovimiento(Dimension& pos){
     for(int i = pos.x()-1; i <= pos.x()+1; i++){
         for(int j = (i==pos.x()) ? pos.y()-1 : pos.y(); (i==pos.x()) ? (j <= pos.y()+1) : (j == pos.y()); j+=2){
             Dimension adyacente(i, j);
-            if(!enRango(adyacente, tamanio)) continue;
+            if(!enRango(adyacente, *tamanio)) continue;
             if(! Matriz::intercambiar(pos, adyacente))
                 continue;
 
@@ -301,8 +309,8 @@ bool Tablero::hayMovimiento(Dimension& pos){
 }
 
 bool Tablero::hayMovimientos(){
-    for(int i=0; i< tamanio; i++){
-        for(int j=0; i< tamanio; j++){
+    for(int i=0; i < tamanio->x(); i++){
+        for(int j=0; i < tamanio->y(); j++){
             Dimension actual(i,j);
             if (hayMovimiento(actual))
                 return true;
@@ -321,7 +329,7 @@ bool Tablero::estabilizar(){
         for(int i = pos->x()-1; i <= pos->x()+1; i++){
             for(int j = (i==pos->x()) ? pos->y()-1 : pos->y(); (i==pos->x()) ? (j <= pos->y()+1) : (j == pos->y()); j+=2){
                 Dimension adyacente(i, j);
-                if(! enRango(adyacente, tamanio)) continue;
+                if(! enRango(adyacente, *tamanio)) continue;
 
                 int orientacion = FILA;
                 bool ultimo = false;
@@ -342,13 +350,13 @@ bool Tablero::estabilizar(){
 
 void Tablero::imprimir(){
     std::cout << "      ";
-    for(int i=0; i<tamanio; i++)
+    for(int i=0; i<tamanio->x(); i++)
         std::cout << i << "     ";
     std::cout << '\n' << '\n';
 
-    for(int i=0; i<tamanio; i++){
+    for(int i=0; i<tamanio->x(); i++){
         std::cout << i << "     ";
-        for(int j=0; j<tamanio; j++){
+        for(int j=0; j<tamanio->y(); j++){
             Celda salida = (*this)[i][j];
             std::cout << '(' << salida.getColor() << ',' << salida.getTipo() << ')';
                 std::cout << "     ";
